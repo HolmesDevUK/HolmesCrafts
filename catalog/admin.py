c5bae048d2ec25389d73e5bbeeba0132295fe19e
HolmesDev <holmesdevuk@gmail.com>
2025-08-19 19:14:52 +0100

    migrations for models in new apps

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,14 +9,14 @@ class NotebookAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ("name", "size", "price", "code",)
     list_filter = ("name", "size", "price",)
 
-@admin.register(SortableAdminMixin, Card)
-class CardAdmin(admin.ModelAdmin):
+@admin.register(Card)
+class CardAdmin(SortableAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name", "code",)}
     list_display = ("name", "code", "size", "price", "card_type")
     list_filter = ("size", "price", "in_store", "has_variants", "card_type")    
 
 @admin.register(CardVariant)
-class CardVariantAdmin(admin.ModelAdmin):
+class CardVariantAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ("card", "variant_name",)
     list_filter = ("card",)  
 
