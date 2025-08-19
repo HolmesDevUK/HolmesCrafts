07bc38096fdab9c0a01ba6f8e6cceef371ef9543
HolmesDev <holmesdevuk@gmail.com>
2025-08-19 16:57:24 +0100

    set up settings

--- HolmesCrafts/settings.py
+++ HolmesCrafts/settings.py
@@ -16,15 +16,25 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env_path = Path('/var/www/envs/HolmesCrafts.env')
+env_path_dev = BASE_DIR / ".env"
+
+env = environ.Env()
+
+if env_path_dev.exists():
+    env.read_env(env_path_dev)
+else:
+    env.read_env(env_path)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uu-=$4nuenv2)cyevjlj#3!8tos77%71qe(6yjk=pzb@@i@qe('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -81,10 +91,7 @@ WSGI_APPLICATION = 'HolmesCrafts.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(default='sqlite:///db.sqlite3')
 }
 
 
@@ -122,11 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -137,4 +146,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 SESSION_COOKIE_AGE = 3600
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_USERNAME")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+
+EMAIL_DISPLAY = f"TaskHub <{EMAIL_HOST_USER}>"
 
