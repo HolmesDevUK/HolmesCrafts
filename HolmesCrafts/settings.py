aa1e0d58609b612b8ff7d55715b6e11ad8022ad9
HolmesDev <holmesdevuk@gmail.com>
2025-08-20 18:58:38 +0100

    add context_processors

--- HolmesCrafts/settings.py
+++ HolmesCrafts/settings.py
@@ -83,6 +83,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.set_nav_bar',
+                'core.context_processors.in_basket',
             ],
         },
     },
