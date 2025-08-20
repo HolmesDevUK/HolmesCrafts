e9a2f8177755c5b9b38ebb3352097aca8f8b7678
HolmesDev <holmesdevuk@gmail.com>
2025-08-20 19:10:26 +0100

    set up navbar context query

--- catalog/services.py
+++ catalog/services.py
@@ -1,2 +1,12 @@
 from .models import Card, Notebook
 
+def get_featured():
+
+    return {
+        "notebooks": Notebook.objects.filter(is_featured=True),
+        "birthday_cards": Card.objects.filter(card_type="Birthday", is_featured=True),
+        "wedding_cards": Card.objects.filter(card_type="Wedding", is_featured=True),
+        "seasonal_cards": Card.objects.filter(card_type="Seasonal", is_featured=True),
+        "special_cards": Card.objects.filter(card_type="Special Occasion", is_featured=True),
+        "other_cards": Card.objects.filter(card_type="Other", is_featured=True),
+    }
--- core/views.py
+++ core/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-from catalog.models import Card, Notebook
+from catalog.services import get_featured
 
 class HomeView(TemplateView):
     template_name = "holmescraftsuk/index.html"
 
     def get_context_data(self, **kwargs):
-        notebooks = Notebook.objects.filter(is_featured=True)
-        birthday_cards = Card.objects.filter(card_type="Birthday").filter(is_featured=True)
-        wedding_cards = Card.objects.filter(card_type="Wedding").filter(is_featured=True)
-        seasonal_cards = Card.objects.filter(card_type="Seasonal").filter(is_featured=True)
-        special_cards = Card.objects.filter(card_type="Special Occasion").filter(is_featured=True)
-        other_cards = Card.objects.filter(card_type="Other").filter(is_featured=True)
         context = super().get_context_data(**kwargs)
-        context["notebooks"] = notebooks
-        context["birthday_cards"] = birthday_cards
-        context["wedding_cards"] = wedding_cards
-        context["seasonal_cards"] = seasonal_cards
-        context["special_cards"] = special_cards
-        context["other_cards"] = other_cards
+        context.update(get_featured())
         context["current_page"] = "HOME"
         return context
