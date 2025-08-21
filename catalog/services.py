4039a4f23df955c599edfa174489551c0b826430
HolmesDev <holmesdevuk@gmail.com>
2025-08-21 14:24:03 +0100

    fix card type

--- catalog/services.py
+++ catalog/services.py
@@ -4,9 +4,9 @@ def get_featured():
 
     return {
         "notebooks": Notebook.objects.filter(is_featured=True),
-        "birthday_cards": Card.objects.filter(card_type="Birthday", is_featured=True),
-        "wedding_cards": Card.objects.filter(card_type="Wedding", is_featured=True),
-        "seasonal_cards": Card.objects.filter(card_type="Seasonal", is_featured=True),
-        "special_cards": Card.objects.filter(card_type="Special Occasion", is_featured=True),
-        "other_cards": Card.objects.filter(card_type="Other", is_featured=True),
+        "birthday_cards": Card.objects.filter(card_type="birthday", is_featured=True),
+        "wedding_cards": Card.objects.filter(card_type="wedding", is_featured=True),
+        "seasonal_cards": Card.objects.filter(card_type="seasonal", is_featured=True),
+        "special_cards": Card.objects.filter(card_type="special", is_featured=True),
+        "other_cards": Card.objects.filter(card_type="other", is_featured=True),
     }
