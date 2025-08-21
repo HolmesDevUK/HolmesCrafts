dd03511ae7e549e2e3d1c3941637fc2f35cdb546
HolmesDev <holmesdevuk@gmail.com>
2025-08-21 13:07:12 +0100

    update template

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 from catalog.services import get_featured
 
 class HomeView(TemplateView):
-    template_name = "holmescraftsuk/index.html"
+    template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
