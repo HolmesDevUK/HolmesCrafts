5552a5c885efcee56544928c9fb49578649bc59c
HolmesDev <holmesdevuk@gmail.com>
2025-08-21 16:54:05 +0100

    remove context from views

--- catalog/views.py
+++ catalog/views.py
@@ -1,45 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Card, Notebook
+from core.models import Navbar
 
 class NotebooksView(ListView):
-    template_name = "holmescraftsuk/notebooks.html"
+    template_name = "catalog/notebooks.html"
     model = Notebook
     context_object_name = "notebooks"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["nav_bar"] = set_nav()
-        context["nav_bar_cards"] = set_nav_cards()
         context["current_page"] = "NOTEBOOKS"
-        context["in_cart"] = in_basket(self.request)
         return context
     
 class CardsView(ListView):
-    template_name = "holmescraftsuk/cards.html"
+    template_name = "catalog/cards.html"
     model = Card
     context_object_name = "cards"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["nav_bar"] = set_nav()
-        context["nav_bar_cards"] = set_nav_cards()
         context["current_page"] = get_object_or_404(Navbar, slug=self.kwargs["slug"])
-        context["in_cart"] = in_basket(self.request)
         return context
     
 class NotebookShopPageView(DetailView):
-    template_name = "holmescraftsuk/notebook_shop_page.html" 
+    template_name = "catalog/notebook_shop_page.html" 
     model = Notebook 
     context_object_name = "notebook"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["nav_bar"] = set_nav()
-        context["nav_bar_cards"] = set_nav_cards()
-        context["in_cart"] = in_basket(self.request)
-        return context
     
     def post(self, request):
         cart = request.session.get("cart", {})
@@ -51,16 +40,9 @@ class NotebookShopPageView(DetailView):
         request.session["cart"] = cart
     
 class CardShopPageView(DetailView):
-    template_name = "holmescraftsuk/card_shop_page.html" 
+    template_name = "catalog/card_shop_page.html" 
     model = Card
     context_object_name = "card"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs) 
-        context["nav_bar"] = set_nav()
-        context["nav_bar_cards"] = set_nav_cards()
-        context["in_cart"] = in_basket(self.request)
-        return context 
     
     def post(self, request):
         cart = request.session.get("cart", {})
