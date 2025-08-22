0e3e39b2119d8dd5ea8fdef902e6057b8ca45c98
HolmesDev <holmesdevuk@gmail.com>
2025-08-23 00:08:59 +0100

    fix for changed models

--- catalog/services.py
+++ catalog/services.py
@@ -4,9 +4,9 @@ def get_featured():
 
     return {
         "notebooks": Notebook.objects.filter(is_featured=True),
-        "birthday_cards": Card.objects.filter(card_type="birthday", is_featured=True),
-        "wedding_cards": Card.objects.filter(card_type="wedding", is_featured=True),
-        "seasonal_cards": Card.objects.filter(card_type="seasonal", is_featured=True),
-        "special_cards": Card.objects.filter(card_type="special", is_featured=True),
-        "other_cards": Card.objects.filter(card_type="other", is_featured=True),
+        "birthday_cards": Card.objects.filter(card_type__name="birthday", is_featured=True),
+        "wedding_cards": Card.objects.filter(card_type__name="wedding", is_featured=True),
+        "seasonal_cards": Card.objects.filter(card_type__name="seasonal", is_featured=True),
+        "special_cards": Card.objects.filter(card_type__name="special", is_featured=True),
+        "other_cards": Card.objects.filter(card_type__name="other", is_featured=True),
     }
