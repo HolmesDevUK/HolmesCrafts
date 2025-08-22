f4443b9e55a9d4d897c902b18f66048bc399cdfa
HolmesDev <holmesdevuk@gmail.com>
2025-08-23 00:32:54 +0100

    fix urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ app_name = "catalog"
 
 urlpatterns = [
     path("notebooks/", views.NotebooksView.as_view(), name="notebooks"),
-    path("<slug:slug>/", views.CardsView.as_view(), name="cards"),
+    path("cards/<slug:slug>/", views.CardsView.as_view(), name="cards"),
     path("notebooks/<slug:slug>/", views.NotebookShopPageView.as_view(), name="notebook_shop_page"),
-    path("<slug:page_slug>/<slug:slug>/", views.CardShopPageView.as_view(), name="card_shop_page")
+    path("cards/<slug:page_slug>/<slug:slug>/", views.CardShopPageView.as_view(), name="card_shop_page")
 ]
