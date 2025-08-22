bca0d6420cc365368e416f03ef6bca20264108f9
HolmesDev <holmesdevuk@gmail.com>
2025-08-22 18:54:05 +0100

    add mixin to models

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,8 @@ import os
 import uuid
 from datetime import datetime
 
+from .mixins import OrderingMixin
+
 def upload_to(instance, filename):
     
     app_name = instance._meta.app_label
@@ -20,7 +22,7 @@ def upload_to(instance, filename):
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
-class Size(models.Model):
+class Size(OrderingMixin, models.Model):
 
     ORIENTATION = (
         ("portrait", "Portrait"),
@@ -39,7 +41,7 @@ class ProductType(models.Model):
     def __str__(self):
         return self.name
 
-class PriceGroup(models.Model):
+class PriceGroup(OrderingMixin, models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -47,7 +49,7 @@ class PriceGroup(models.Model):
     def __str__(self):
         return f"{self.product_type} - ({self.size}) - £{self.price}"
     
-class Product(models.Model):
+class Product(OrderingMixin, models.Model):
     name = models.CharField(max_length=100, default="")
     price_group = models.ForeignKey(PriceGroup, on_delete=models.PROTECT, related_name="products")
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
@@ -56,13 +58,9 @@ class Product(models.Model):
     in_store = models.BooleanField(default=True)
     has_variants = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="products", blank=True)
-    order = models.PositiveIntegerField(default=0, editable=False)
 
     def __str__(self):
         return f"{self.name} ({self.code})"
-    
-    class Meta:
-        ordering = ["order"]
 
     @property
     def price(self):
@@ -128,16 +126,14 @@ class CardImg(models.Model):
     class Meta:
         verbose_name_plural = "Card images"
 
-class CardVariant(models.Model):
+class CardVariant(OrderingMixin, models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="variants")
     variant_name = models.CharField(max_length=100)
     variant_img = models.ImageField(upload_to=upload_to)
-    order = models.PositiveIntegerField(default=0, editable=False)
 
     def __str__(self):
         return f"{self.card} ({self.variant_name})"
 
     class Meta:
-        ordering = ["order"]
         verbose_name_plural = "Card variants"
 
