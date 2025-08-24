ab9aa1b9b0cf4d74f4da1684f3caa3334851e8d2
HolmesDev <holmesdevuk@gmail.com>
2025-08-24 11:44:58 +0100

    fix bugs

--- catalog/services.py
+++ catalog/services.py
@@ -5,7 +5,7 @@ def get_featured():
     return {
         "notebooks": Notebook.objects.filter(is_featured=True).select_related("images"),
         "birthday_cards": Card.objects.filter(card_type__name="Birthday", is_featured=True).select_related("images"),
-        "wedding_cards": Card.objects.filter(card_type__name="Wedding", is_featured=True).select_related("images"),
+        "wedding_cards": Card.objects.filter(card_type__name="Wedding & Anniversary", is_featured=True).select_related("images"),
         "seasonal_cards": Card.objects.filter(card_type__name="Seasonal", is_featured=True).select_related("images"),
         "special_cards": Card.objects.filter(card_type__name="Special Occasion", is_featured=True).select_related("images"),
         "other_cards": Card.objects.filter(card_type__name="Other", is_featured=True).select_related("images"),
