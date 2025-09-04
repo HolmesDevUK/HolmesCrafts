27436a0b85a25d83a74d9641f65810c3904fa2a1
HolmesDev <holmesdevuk@gmail.com>
2025-09-04 16:57:20 +0100

    cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,5 @@ app_name = "cart"
 urlpatterns = [
     path("", BasketView.as_view(), name="basket"),
     path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('remove/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
 ]
