9aac7cb2dc4229e51202ae1d01f79dced32a0522
HolmesDev <holmesdevuk@gmail.com>
2025-09-10 16:49:00 +0100

    register forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegisterUserForm
+from .models import CustomUser
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = RegisterUserForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("core:home_page")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object) 
+        return response
