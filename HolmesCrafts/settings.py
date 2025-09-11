ca23e5c4e8da93c36aed6d7eb452d0e3049bed53
HolmesDev <holmesdevuk@gmail.com>
2025-09-11 15:52:36 +0100

    logout

--- HolmesCrafts/settings.py
+++ HolmesCrafts/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "core:home_page"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", views.RegisterView.as_view(), name="register")
+    path("register/", views.RegisterView.as_view(), name="register"),
+    # path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
