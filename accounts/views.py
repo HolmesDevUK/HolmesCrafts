f2805b653ef93c946365b9713cad867610e8bbd3
HolmesDev <holmesdevuk@gmail.com>
2025-09-16 18:54:57 +0100

    alert messages

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView
 
 from .forms import RegisterUserForm, LoginForm
 from .models import CustomUser
-from core.utils import new_user_email_admin, new_user_confirmation
+from core.utils import new_user_email_admin, new_user_confirmation, alert_success
 
 class RegisterView(CreateView):
     model = CustomUser
@@ -18,6 +18,11 @@ class RegisterView(CreateView):
 
         new_user_confirmation(user = self.object)
         new_user_email_admin(user = self.object)
+
+        alert_success(
+            self.request,
+            f"Welcome, {self.object.name}. Your account has been successfully created!"
+        )
 
         login(self.request, self.object) 
         return response
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,4 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home_page"),
-    path("home/", views.home)
 ]
