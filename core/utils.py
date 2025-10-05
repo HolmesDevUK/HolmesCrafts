6be65941e0507cbf1eb9948d261c7bc71d04635d
HolmesDev <holmesdevuk@gmail.com>
2025-10-05 17:54:49 +0100

    stripe product helper

--- core/utils.py
+++ core/utils.py
@@ -5,6 +5,27 @@ from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.contrib import messages
+import stripe
+
+stripe.api_key = settings.STRIPE_SK
+
+def get_or_create_stripe_product(product):
+    if product.stripe_product_id:
+        return product.stripe_product_id
+    
+    stripe_product = stripe.Product.create(
+        name=product.name,
+        description=product.description,
+        images=[product.image_url] if product.image_url else None,
+        metadata={"product_id": product.id},
+    )
+
+    product.stripe_product_id = stripe_product.id
+    product.save(update_fields=["stripe_product_id"])
+
+    return stripe_product.id
+
+
 
 def absolute_url(url:str, request=None) -> str:
     if not url:
