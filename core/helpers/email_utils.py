1ee307e32e67b0c4b48681591c1acd4ed9567a69
HolmesDev <holmesdevuk@gmail.com>
2025-10-09 15:44:52 +0100

    order confirmation admin set up

--- core/helpers/email_utils.py
+++ core/helpers/email_utils.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+my_email = settings.MY_EMAIL
+from_email = settings.EMAIL_DISPLAY
+
 def send_email(subject: str, to_email: str, template_name: str, context: dict = None):
 
     logo_url = f"{settings.SITE_URL}{settings.STATIC_URL}core/images/logo.png"
@@ -21,9 +24,9 @@ def send_email(subject: str, to_email: str, template_name: str, context: dict = 
     email = EmailMultiAlternatives(
             subject = subject,
             body = html_content,
-            from_email = settings.EMAIL_DISPLAY,
+            from_email = from_email,
             to = [to_email],
-            reply_to = [settings.MY_EMAIL],
+            reply_to = [my_email],
         )
     
     email.attach_alternative(html_content, "text/html")
@@ -33,7 +36,7 @@ def new_user_email_admin(user):
 
     send_email(
         subject = "New User",
-        to_email = settings.MY_EMAIL,
+        to_email = my_email,
         template_name = "core/emails/simple_admin_notification.html",
         context = {
             "subject": "A New User has Registered",
@@ -50,3 +53,11 @@ def new_user_confirmation(user):
             "user": user,
         }
     )        
+
+def order_confirmation_admin(data):
+    send_email(
+        subject = "New Order",
+        to_email = my_email,
+        template_name = "core/emails/order_confirmation_admin.html",
+        context = data,
+    )   
