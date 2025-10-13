50bb0a8784aeb8e8319db40d388d98155ddde82c
HolmesDev <holmesdevuk@gmail.com>
2025-10-13 14:48:03 +0100

    update delete carts helper function

--- core/helpers/clean_up.py
+++ core/helpers/clean_up.py
@@ -6,7 +6,7 @@ from cart.models import Cart
 def delete_old_carts(days=7):
 
     cutoff_date = timezone.now() - timedelta(days=days)
-    old_carts = Cart.objects.filter(user__isnull=True, updated_at_lt=cutoff_date)
+    old_carts = Cart.objects.filter(user__isnull=True, updated_at__lt=cutoff_date)
     count = old_carts.count()
     old_carts.delete()
     return count
