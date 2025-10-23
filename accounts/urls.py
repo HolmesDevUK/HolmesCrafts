62a21ba79422d028226cc4f14b3b6f9a49828560
HolmesDev <holmesdevuk@gmail.com>
2025-10-23 15:51:46 +0100

    set up password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
